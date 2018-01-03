da8f7b8141c7c3c5a1896284b6b0080139f5bac2
Owais <owaiswiz@gmail.com>
2018-01-03 20:29:04 +0530

    Await for getThreads to complete before executing tests

--- app/api/__tests__/Threads.test.js
+++ app/api/__tests__/Threads.test.js
@@ -1,19 +1,26 @@
 import { getThreads } from '../Threads';
 
-const threads = getThreads("/g/");
+let threads;
+
+beforeAll(async () => {
+  threads = await getThreads("g");
+});
+
 describe('getThreads', () => {
   it('returns a non-empty array', () => {
     expect(Array.isArray(threads)).toBe(true);
     expect(threads.length).toBeGreaterThan(0);
   });
-  it('returns thread with all valid fields', () => {
+
+  it('returns atleast one valid thread', () => {
     const thread = threads[0];
     expect(thread.no).toBeDefined();
     expect(thread.title || thread.text).toBeDefined();
+    expect(thread.name).toBeDefined();
     expect(thread.createdAt).toBeDefined();
     expect(thread.lastModifiedAt).toBeDefined();
-    expect(thread.noOfReplies).toBeGreaterThanEqual(0);
-    expect(thread.noOfImages).toBeGreaterThanEqual(0);
+    expect(thread.noOfReplies).toBeGreaterThanOrEqual(0);
+    expect(thread.noOfImages).toBeGreaterThanOrEqual(0);
     expect(thread.imageURL).toMatch(/\./);
     expect(thread.thumbnailURL).toMatch(/jpg/);
   });
